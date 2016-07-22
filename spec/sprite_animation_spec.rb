d3af69043a0e0aa19b273d5e2c39060664c32778
DanielsLuz <danielluz1995@gmail.com>
2016-07-22 17:44:56 -0300

    Adding initial fixtures and sample tests.

--- spec/sprite_animation_spec.rb
+++ spec/sprite_animation_spec.rb
@@ -1,6 +1,21 @@
 require 'spec_helper'
 
+include SpriteAnimation
+
 describe SpriteAnimation do
-  pending "write it"
+
+  context 'using ImageSize util' do
+    it 'should return sizes as expected' do
+      GoodSheets.each do |name, info|
+        expect(info).to match_array(ImageSize.size(TestUrl + name))
+      end
+    end
+  end
+
+  context 'calling method' do
+    xit 'should return correct string' do
+      expect(animate_sprite(TestUrl + "coin1.png", "10")).to be_equal("empty")
+    end
+  end
 end
 
